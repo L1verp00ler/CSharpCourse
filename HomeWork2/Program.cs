7197d3ff6e68fc7c0b9476119c1c5e3b99efaa84
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-28 16:59:54 +0300

    ДЗ №2: 1. Создать проект HomeWork2 в том же solution (консольное приложение) 2. Написать программу, которая делает следующее: 2.1. Запрашивает у пользователя целое число 2.2. Генерирует массив случайных чисел размером, заданным введенным числом 2.3. Выводит полученный массив на консоль 2.4. Сортирует массив  алгоритмом пузырьковой сортировки 2.5. Выводит отсортированный массив на консоль

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number;
+
+            if (!TryEnterNumberFromConsole(out number))
+            {
+                Console.Read();
+                return;
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Число должно быть больше нуля!");
+                Console.Read();
+                return;
+            }
+
+            int[] unsortedNumbersArray = GenerateNumbersArrayOfSize(number);
+
+            Console.WriteLine("Сгенерированный массив случайных чисел размером, заданным введенным числом: ");
+            PrintArray(unsortedNumbersArray);
+
+            int[] sortedNumbersArray = BubbleSortOfArray(unsortedNumbersArray);
+
+            Console.WriteLine("Отсортированный массив чисел: ");
+            PrintArray(sortedNumbersArray);
+
+            Console.ReadLine();
+        }
+
+        static bool TryEnterNumberFromConsole(out uint number)
+        {
+            Console.Write("Введите целое число: ");
+
+            string numberAsString = Console.ReadLine();
+
+            if (!(uint.TryParse(numberAsString, out number)))
+            {
+                Console.Write("Ввели не число!");
+                return false;
+            }
+
+            return true;
+        }
+
+        static int[] GenerateNumbersArrayOfSize(uint number)
+        {
+            int[] numbersArray = new int[number];
+            Random rnd = new Random();
+            for (int i = 0; i < number; i++)
+            {
+                numbersArray[i] = rnd.Next();
+            }
+            return numbersArray;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        // Сортировка массива алгоритмом пузырьковой сортировки
+        // Будет идти до определенного элемента (так как последнее число и 'n'[0,1,...,n] чисел до него уже можно не сравнивать)
+        static int[] BubbleSortOfArray(int[] numbersArray)
+        {
+            for (int i = 0; i < numbersArray.Length - 1; i++)
+            {
+                for (int j = 0; j < numbersArray.Length - i - 1; j++)
+                {
+                    if (numbersArray[j] > numbersArray[j + 1])
+                    {
+                        int tempElem = numbersArray[j + 1];
+                        numbersArray[j + 1] = numbersArray[j];
+                        numbersArray[j] = tempElem;
+                    }
+                }
+            }
+
+            return numbersArray;
+        }
+    }
+}
