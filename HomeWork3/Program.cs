50b3d2e275b0a526b02aa35169018341074b0643
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-29 18:46:57 +0300

    ДЗ №3: - добавил в комментарий к методу переворота строки ссылку на классный пример по его реализации

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -82,6 +82,7 @@ namespace HomeWork3
             return words.Length;
         }
 
+        // http://ru.stackoverflow.com/questions/146083/%D0%9A%D0%B0%D0%BA-%D0%BD%D0%B0-c-%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%83%D1%82%D1%8C-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D1%83-%D0%B1%D1%8B%D0%BB%D0%BE-123-%D1%81%D1%82%D0%B0%D0%BB%D0%BE-321
         static string StringReverse(string stringFromUser)
         {
             string reverseString = "";
