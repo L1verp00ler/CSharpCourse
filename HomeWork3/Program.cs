58f92b6062ed1dfe59193cd4950e185085e710b8
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-03-29 17:55:33 +0300

    ДЗ №3: - добавил ввод строки пользователем; - добавил метод, проверяющий является ли строка палиндромом.

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork3
 {
@@ -10,6 +6,66 @@ namespace HomeWork3
     {
         static void Main(string[] args)
         {
+            //string stringFromUser;
+
+            Console.Write("Введите какой-нибудь текст: ");
+
+            string stringFromUser = Console.ReadLine();
+
+            if (stringFromUser.Length == 0)
+            {
+                Console.WriteLine("Строка не должна быть пустой!");
+                Console.Read();
+                return;
+            }
+
+            Console.WriteLine(stringFromUser.Length);
+
+            if (IsPalindrom(stringFromUser))
+            {
+                Console.WriteLine("Строка является палиндромом!");
+            } else
+            {
+                Console.WriteLine("Строка не является палиндромом!");
+            }
+
+            Console.Read();
+        }
+
+        static bool IsPalindrom(string stringFromUser)
+        {
+            if (stringFromUser.Length%2 == 0)
+            {
+                Console.WriteLine("Четное число символов в строке!");
+                for (int i = 0, j = stringFromUser.Length - 1; i < stringFromUser.Length / 2; i++, j--)
+                {
+                    if (stringFromUser[i] != stringFromUser[j])
+                    {
+                        Console.WriteLine("{0} не равен {1}", stringFromUser[i], stringFromUser[j]);
+                        return false;
+                    } else
+                    {
+                        Console.WriteLine("{0} равен {1}", stringFromUser[i], stringFromUser[j]);
+                    }
+                }
+                return true;
+            } else 
+            {
+                Console.WriteLine("Нечетное число символов в строке!");
+                for (int i = 0, j = stringFromUser.Length - 1; i < (stringFromUser.Length - 1) / 2; i++, j--)
+                {
+                    if (stringFromUser[i] != stringFromUser[j])
+                    {
+                        Console.WriteLine("{0} не равен {1}", stringFromUser[i], stringFromUser[j]);
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} равен {1}", stringFromUser[i], stringFromUser[j]);
+                    }
+                }
+                return true;
+            }
         }
     }
 }
