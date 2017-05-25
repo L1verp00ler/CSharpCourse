e8f648c1d1b30742772d324fd8e88616ce449e86
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-05-25 18:20:32 +0300

    ДЗ №9: - первые наработки.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbersList = new List<double>();
+            List<char> operaionsList = new List<char>();
+            string numberAsString = "";
+
+            Console.WriteLine("Введите выражение:");
+            string expression = Console.ReadLine();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                while (expression[i] == '/' || expression[i] == '*' || expression[i] == '+' || expression[i] == '-')
+                {
+                    numberAsString += expression[i];
+                }
+
+                //numbersList.Add((double)numberAsString);
+
+                if (expression[i] == '/' || expression[i] == '*' || expression[i] == '+' || expression[i] == '-')
+                {
+                    operaionsList.Add(expression[i]);
+                }
+            }
+
+            Console.WriteLine(expression[0]);
+            Console.WriteLine(expression[1]);
+
+            Console.WriteLine(expression);
+            Console.Read();
+
+            /*
+            // Пример алгоритма сложности O(n^2)
+            // Сортировка массива алгоритмом пузырьковой сортировки
+            // Будет идти до определенного элемента (так как последнее число и 'n'[0,1,...,n] чисел до него уже можно не сравнивать)
+            static int[] BubbleSortOfArray(int[] numbersArray)
+            {
+                for (int i = 0; i < numbersArray.Length - 1; i++)
+                {
+                    for (int j = 0; j < numbersArray.Length - i - 1; j++)
+                    {
+                        if (numbersArray[j] > numbersArray[j + 1])
+                        {
+                            int tempElem = numbersArray[j + 1];
+                            numbersArray[j + 1] = numbersArray[j];
+                            numbersArray[j] = tempElem;
+                        }
+                    }
+                }
+
+                return numbersArray;
+            }
+            */
+        }
+    }
+}
