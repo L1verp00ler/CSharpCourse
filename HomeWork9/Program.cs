5a530906f767e49b818bef019f4be29164278f0c
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-01 18:55:33 +0300

    ДЗ №9: - рабочий вариант парсера выражений.

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -8,13 +8,155 @@ namespace HomeWork9
 {
     class Program
     {
-        static int OptionalParam(int x, int y, int z = 5, int s = 4)
+        enum Operation
         {
-            return x + y + z + s;
+            Plus = 1,
+            Minus,
+            Multiply,
+            Divide
+        }
+
+        static double Calculate(double oper1, double oper2, Operation op)
+        {
+            double result;
+
+            switch (op)
+            {
+                case Operation.Plus:
+                    result = oper1 + oper2;
+                    break;
+                case Operation.Minus:
+                    result = oper1 - oper2;
+                    break;
+                case Operation.Multiply:
+                    result = oper1 * oper2;
+                    break;
+                case Operation.Divide:
+                    result = oper1 / oper2;
+                    break;
+                default:
+                    result = 0.0; // на самом деле тут правильнее кидать исключение!!!
+                    break;
+            }
+
+            return result;
+        }
+
+        static double ParseExpression(string expression)
+        {
+            Operation operation;
+            double result = default(Double);
+            int position;
+            int positionPlus = expression.IndexOf('+');
+            int positionMinus = expression.IndexOf('-');
+            int positionMultiply = expression.IndexOf('*');
+            int positionDivide = expression.IndexOf('/');
+
+            Console.WriteLine(positionPlus);
+            Console.WriteLine(positionMinus);
+            Console.WriteLine(positionMultiply);
+            Console.WriteLine(positionDivide);
+
+            //Console.WriteLine(positionPlus + ' ' + positionMinus + ' ' + positionMultiply + ' ' + positionDivide);
+
+            if (positionPlus > 0)
+            {
+                operation = Operation.Plus;
+                position = positionPlus;
+
+            }
+            else if (positionMinus > 0)
+            {
+                operation = Operation.Minus;
+                position = positionMinus;
+            }
+            else if (positionMultiply > 0)
+            {
+                operation = Operation.Multiply;
+                position = positionMultiply;
+            }
+            else if (positionDivide > 0)
+            {
+                operation = Operation.Divide;
+                position = positionDivide;
+            }
+            else
+            {
+                operation = default(Operation); // 0
+                position = 0;
+            }
+
+
+            string oper1 = expression.Remove(position);
+            string oper2 = expression.Remove(0, position + 1);
+            Console.WriteLine(oper1);
+            Console.WriteLine(oper2);
+
+            Double.TryParse(oper1, out double operand1);
+            Console.WriteLine(operand1);
+            Double.TryParse(oper2, out double operand2);
+            Console.WriteLine(operand2);
+
+            if (operand1 == 0)
+            {
+                operand1 = ParseExpression(oper1);
+            }
+
+            if (operand2 == 0)
+            {
+                operand2 = ParseExpression(oper2);
+            }
+            
+            result = Calculate(operand1, operand2, operation);
+            /*
+            if (operand1 != 0 && operand2 != 0)
+            {
+                result = Calculate(operand1, operand2, operation);
+            }
+            */
+
+            return result;
+            
+            //double chislo = Double.Parse(oper1);
+            //Console.WriteLine(chislo);
+
+            //Decimal.TryParse(inputString, out Decimal result)); // рабочая штуковина!!!
+
+            //if ()
+
+            /*
+            if (positionPlus != 0 && positionMinus != 0)
+            {
+                Operation operation = positionPlus < positionMinus ? Operation.Plus : Operation.Minus;
+                int position = positionPlus < positionMinus ? positionPlus : positionMinus;
+            }
+            //Console.WriteLine(inputString);
+            */
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите выражение: ");
+            //string inputString = Console.ReadLine();
+            string inputString = "7,1+3*5/10-7*2/4+5";
+            Console.WriteLine(ParseExpression(inputString));
+
+            /*
+            //double result = 
+            char[] operations = new char[] { '*', '/', '+', '-' };
+            int positionPlus = inputString.IndexOf('+');
+            int positionMinus = inputString.IndexOf('-');
+            if (positionPlus != 0 && positionMinus != 0)
+            {
+                Operation operation = positionPlus < positionMinus ? Operation.Plus : Operation.Minus;
+                int position = positionPlus < positionMinus ? positionPlus : positionMinus;
+            }
+            Console.WriteLine(inputString);
+            */
+
+
+
+            /*
             string inputString = Console.ReadLine();
             //string inputString = "HelloWorld";
             char symbol = 'W';
@@ -24,6 +166,7 @@ namespace HomeWork9
             Console.WriteLine(inputString.All(Char.IsNumber));
             Console.WriteLine(positionNumber);
             Console.Read();
+            */
 
             /*
             Console.WriteLine("Нажмите клавишу 'Esc'!");
