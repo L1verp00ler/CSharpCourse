4b274bcd2b75301b084f814ae8ca574049307c75
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-13 01:49:04 +0300

    Урок №7 (промежуточная аттестация): - 3. Сложение больших чисел, переданных в формате строки (сравнение длины у входящих строк и дополнение строки меньшей длины нулями до размера большей).

--- Lesson7_TestTask3/Program.cs
+++ Lesson7_TestTask3/Program.cs
@@ -10,12 +10,24 @@ namespace Lesson7_TestTask3
     {
         static void Main(string[] args)
         {
+            /// Array reverse!!!
+            string resultString = "";
+
             Console.Write("Введите первое большое число: ");
             string number1AsString = Console.ReadLine();
 
             Console.Write("Введите второе большое число: ");
             string number2AsString = Console.ReadLine();
 
+            // Проверка на ввод пустой строки
+            if (number1AsString == "" || number2AsString == "")
+            {
+                Console.WriteLine("Вы не ввели число(а)!");
+                Console.Read();
+                return;
+            }
+
+            // Проверка на ввод недопустимых символов
             if (!number1AsString.All(Char.IsDigit) || !number2AsString.All(Char.IsDigit))
             {
                 Console.WriteLine("Вы допустили ошибку при вводе чисел!");
@@ -23,7 +35,36 @@ namespace Lesson7_TestTask3
                 return;
             }
 
-            Console.WriteLine(number1AsString + number2AsString);
+            Console.WriteLine(number1AsString.Length);
+            Console.WriteLine(number2AsString.Length);
+
+            //int minNumber = 0;
+            int difference = 0;
+
+
+            if (number1AsString != number2AsString)
+            {
+                difference = Math.Abs(number1AsString.Length - number2AsString.Length);
+                Console.WriteLine(difference);
+
+                string additionalStr = new String('0', difference);
+                Console.WriteLine(additionalStr);
+
+                if (number1AsString.Length < number2AsString.Length)
+                {
+                    number1AsString = additionalStr + number1AsString;
+                }
+                else
+                {
+                    number2AsString = additionalStr + number2AsString;
+                }
+            }
+
+
+
+            //Console.WriteLine(minNumber);
+
+            Console.WriteLine(number1AsString + ", " + number2AsString);
             Console.Read();
         }
     }
