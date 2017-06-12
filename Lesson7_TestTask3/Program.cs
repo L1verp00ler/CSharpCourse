30d7a8ef2365adca3c594598c9a8cbc47b90044f
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-13 02:22:21 +0300

    Урок №7 (промежуточная аттестация): - 3. Сложение больших чисел, переданных в формате строки (Пока что поддерживает сложение чисел в сумме, дающих не больше 9!!! (т.е. не реализован перенос по разрядам)).

--- Lesson7_TestTask3/Program.cs
+++ Lesson7_TestTask3/Program.cs
@@ -35,20 +35,20 @@ namespace Lesson7_TestTask3
                 return;
             }
 
-            Console.WriteLine(number1AsString.Length);
-            Console.WriteLine(number2AsString.Length);
+            Console.WriteLine("Длина первой строки: " + number1AsString.Length);
+            Console.WriteLine("Длина второй строки: " + number2AsString.Length);
 
             //int minNumber = 0;
             int difference = 0;
 
-
+            // Если строки разной длины, то дополняем строку меньшей длины нулями до длины второй строки
             if (number1AsString != number2AsString)
             {
                 difference = Math.Abs(number1AsString.Length - number2AsString.Length);
-                Console.WriteLine(difference);
+                Console.WriteLine("Разница между строками (число символов): " + difference);
 
                 string additionalStr = new String('0', difference);
-                Console.WriteLine(additionalStr);
+                Console.WriteLine("Добавочная строка (нули): " + additionalStr);
 
                 if (number1AsString.Length < number2AsString.Length)
                 {
@@ -58,14 +58,36 @@ namespace Lesson7_TestTask3
                 {
                     number2AsString = additionalStr + number2AsString;
                 }
+
+                Console.WriteLine(number1AsString + ", " + number2AsString);
             }
 
+            /// Пока что поддерживает сложение чисел в сумме, дающих не больше 9!!! (т.е. не реализован перенос по разрядам)
+            // Теперь строки равны и мы можем выполнять преобразования (возможно, можно было и без этого)
+            for (int i = number1AsString.Length - 1; i >= 0; i--)
+            {
+                int sum = Int32.Parse(number1AsString[i].ToString()) + Int32.Parse(number2AsString[i].ToString());
+                Console.WriteLine(sum);
+                resultString += sum.ToString();
+            }
 
+            resultString = StringReverse(resultString);
 
-            //Console.WriteLine(minNumber);
-
-            Console.WriteLine(number1AsString + ", " + number2AsString);
+            Console.WriteLine(resultString);
             Console.Read();
+        }
+
+        // Переворот строки
+        static string StringReverse(string inputString)
+        {
+            string reverseString = "";
+
+            for (int i = inputString.Length - 1; i > -1; i--)
+            {
+                reverseString += inputString[i];
+            }
+
+            return reverseString;
         }
     }
 }
