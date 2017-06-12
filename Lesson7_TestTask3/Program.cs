7e1f9007dc8f4c42059330da85fd8c30d662063b
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-12 18:31:09 +0300

    Урок №7 (промежуточная аттестация): - 3. Сложение больших чисел, переданных в формате строки (добавлена проверка на корректный ввод чисел).

--- Lesson7_TestTask3/Program.cs
+++ Lesson7_TestTask3/Program.cs
@@ -10,7 +10,21 @@ namespace Lesson7_TestTask3
     {
         static void Main(string[] args)
         {
-            //
+            Console.Write("Введите первое большое число: ");
+            string number1AsString = Console.ReadLine();
+
+            Console.Write("Введите второе большое число: ");
+            string number2AsString = Console.ReadLine();
+
+            if (!number1AsString.All(Char.IsDigit) || !number2AsString.All(Char.IsDigit))
+            {
+                Console.WriteLine("Вы допустили ошибку при вводе чисел!");
+                Console.Read();
+                return;
+            }
+
+            Console.WriteLine(number1AsString + number2AsString);
+            Console.Read();
         }
     }
 }
