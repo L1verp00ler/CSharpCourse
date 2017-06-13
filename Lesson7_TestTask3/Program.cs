0234ca3a292753c775cd577b2346f2c115ba0a11
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-13 16:41:30 +0300

    Урок №7 (промежуточная аттестация): - 3. Сложение больших чисел, переданных в формате строки (Рефакторинг - убрал отладку, добавил комменты).

--- Lesson7_TestTask3/Program.cs
+++ Lesson7_TestTask3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson7_TestTask3
 {
@@ -10,7 +7,6 @@ namespace Lesson7_TestTask3
     {
         static void Main(string[] args)
         {
-            /// Array reverse!!!
             string resultString = "";
 
             Console.Write("Введите первое большое число: ");
@@ -30,24 +26,19 @@ namespace Lesson7_TestTask3
             // Проверка на ввод недопустимых символов (буквы, знаки препинания и прочая хрень)
             if (!number1AsString.All(Char.IsDigit) || !number2AsString.All(Char.IsDigit))
             {
-                Console.WriteLine("Вы допустили ошибку при вводе чисел!");
+                Console.WriteLine("Вы ввели не число(а)!");
                 Console.Read();
                 return;
             }
 
-            Console.WriteLine("Длина первой строки: " + number1AsString.Length);
-            Console.WriteLine("Длина второй строки: " + number2AsString.Length);
-
-            int difference = 0;
+            int difference = 0; // 1
 
             // Если строки разной длины, то дополняем строку меньшей длины нулями до длины второй строки
             if (number1AsString.Length != number2AsString.Length)
             {
                 difference = Math.Abs(number1AsString.Length - number2AsString.Length); // вычисляем разницу в кол-ве символов между строками
-                Console.WriteLine("Разница между строками (число символов): " + difference);
 
-                string additionalStr = new String('0', difference); // формируем добавочную строку из нулей линой difference символов
-                Console.WriteLine("Добавочная строка (нули): " + additionalStr);
+                string additionalStr = new String('0', difference); // формируем добавочную строку из нулей длиной difference символов
 
                 // соединяем добавочную строку с строкой меньшей длины, чтобы строки стали одинаковой длины
                 if (number1AsString.Length < number2AsString.Length)
@@ -58,35 +49,29 @@ namespace Lesson7_TestTask3
                 {
                     number2AsString = additionalStr + number2AsString;
                 }
-
-                Console.WriteLine(number1AsString + ", " + number2AsString);
             }
 
-            int sum = 0;
-            int digit = 0;
+            int sum = 0; // 2
+            int digit = 0; // 3
             int box = 0; // По сути, вместо int можно использовать bool (хотяяя, стоит ли?)
             
             // Теперь строки равны и мы можем выполнять преобразования (возможно, можно было и без этого)
             for (int i = number1AsString.Length - 1; i >= 0; i--)
             {
+                // складываем текущие цифры из 2-ух строк
                 sum = Int32.Parse(number1AsString[i].ToString()) + Int32.Parse(number2AsString[i].ToString());
+                // берем от числа (суммы цифр) младший разряд и добавляем к нему перенос
                 digit = sum % 10 + box;
-                int chislo = sum % 10;
-
-                Console.WriteLine("Остаток от деления суммы двух текущих цифр на 10: " + chislo);
-                Console.WriteLine("Сумма двух текущих цифр: " + sum);
-                Console.WriteLine("Сумма цифр с учетом переноса из младшего разряда (и без учета переноса в старший разряд): " + digit);
-
+                // если сумма текущих цифр > 10, то в хранилище помещаем 1, если нет, то 0 (для переноса в старший разряд)
                 box = sum >= 10 ? 1 : 0;
-
-                Console.WriteLine("Перенос в старший разряд: " + box);
-                Console.WriteLine("-----");
 
                 resultString += digit.ToString();
             }
 
+            // так как цикл идет только по длине строк, то в конце нужно проверить, есть ли перенос в старший разряд
             resultString = box > 0 ? resultString += box : resultString;
 
+            // Переворачиваем полученную строку
             resultString = StringReverse(resultString);
 
             Console.WriteLine(resultString);
