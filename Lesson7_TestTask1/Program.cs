1c9facd19ac29ecbc64448cec294764bc48c45d6
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-06-14 17:25:02 +0300

    Урок №7 (промежуточная аттестация): - 1. Умножение факториалов (решение задачи "в лоб").

--- Lesson7_TestTask1/Program.cs
+++ Lesson7_TestTask1/Program.cs
@@ -10,7 +10,56 @@ namespace Lesson7_TestTask1
     {
         static void Main(string[] args)
         {
-            // Тут будет программа для умножения факториалов!!!
+            uint number = 0;
+            
+            Console.Write("Введите натуральное число, большее 0: ");
+
+            try
+            {
+                number = UInt32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод!");
+                Console.Read();
+                return;
+            }
+
+            if (number == 0)
+            {
+                Console.WriteLine("Число должно быть больше нуля!");
+                Console.Read();
+                return;
+            }
+
+            uint factorial = Factorial(number);
+            uint factorialMultiply = FactorialMultiply(number);
+
+            Console.WriteLine("Факториал " + number + " равен " + factorial + ".");
+            Console.WriteLine("Произведение факториалов чисел от 1 до " + number + " равно " + factorialMultiply + ".");
+            Console.Read();
+        }
+
+        // Произведение факториалов чисел от 1 до n
+        static uint FactorialMultiply(uint number)
+        {
+            uint result = 1;
+            for (uint i = 1; i < number + 1; i++)
+            {
+                result *= Factorial(i);
+            }
+            return result;
+        }
+
+        // Вычисление факториала числа
+        static uint Factorial(uint number)
+        {
+            uint result = 1;
+            for (uint i = 1; i < number + 1; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
