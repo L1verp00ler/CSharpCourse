726a56b61c499dd8598f416d62cf927e8d9f97e7
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-07-01 16:02:46 +0300

    Итоговый экзамен. Задачи (2 шт.).

---
+++ Exam_Task_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number = 0;
+            uint result;
+
+            Console.WriteLine("Программа для преобразование целого числа к цифре.");
+            Console.Write("Введите целое число: ");
+
+            try
+            {
+                number = UInt32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод!");
+                Console.Read();
+                return;
+            }
+
+            if (number / 10 == 0)
+            {
+                Console.WriteLine(number);
+                Console.Read();
+            }
+            else
+            {
+                result = TransformNumberInDigit(number);
+                Console.WriteLine(result);
+                Console.Read();
+            }
+        }
+
+        static uint TransformNumberInDigit(uint number)
+        {
+            uint resultNumber = 0;
+            for (int i = 0; i < number.ToString().Length; i++)
+            {
+                //Console.WriteLine(number.ToString()[i]);
+                
+                resultNumber += UInt32.Parse(number.ToString()[i].ToString());
+            }
+            if (resultNumber / 10 == 0)
+            {
+                return resultNumber;
+            }
+            else
+            {
+                return TransformNumberInDigit(resultNumber);
+            }
+            //
+            //return 0;
+        }
+    }
+}
---
+++ Exam_Task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int result;
+
+            Console.WriteLine("Программа для перемножения чисел, возведенных в степень значением того же числа.");
+            Console.Write("Введите целое число: ");
+
+            try
+            {
+                number = Int32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод!");
+                Console.Read();
+                return;
+            }
+
+            result = NumberPowMethod(number);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        static int NumberPowMethod(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * (int)Math.Pow(i, i);
+            }
+            
+            return result;
+        }
+    }
+}
