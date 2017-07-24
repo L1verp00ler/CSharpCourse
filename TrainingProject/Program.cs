87381cc63c7c792af28b9715a72970aeab31f312
L1verp00ler <andrey.golubev.1991@mail.ru>
2017-07-24 22:54:26 +0300

    Учебный проект: - делегаты.

---
+++ TrainingProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingProject
+{
+    class Program
+    {
+        delegate void GetMessage(); // Объявляем делегат
+
+        static void Main(string[] args)
+        {
+            GetMessage del; // Создаем переменную делегата
+
+            if (DateTime.Now.Hour < 12)
+            {
+                del = GoodMorning; // Присваиваем переменной адрес метода
+            }
+            else
+            {
+                del = GoodEvening;
+            }
+
+            del.Invoke();
+            Console.ReadLine();
+        }
+
+        private static void GoodMorning()
+        {
+            Console.WriteLine("Good Morning!");
+        }
+
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Good Evening!");
+        }
+
+    }
+}
